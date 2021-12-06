87dcda5637d38caca52fc1c97d90bdb55ec9056a
Reinout Nonhebel <github@rpnonhebel.nl>
2021-12-06 14:42:17 +0100

    Include aim for day 2 step 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -4,17 +4,18 @@ f = open("Day2/input.txt",'r')
 
 pos = 0
 depth = 0
+aim = 0
 for line in f.readlines():
     cmd, arg = line.split()
 
     if cmd == "forward":
         pos += int(arg)
+        depth += aim * int(arg)
     elif cmd == "down":
-        depth += int(arg)
+        aim += int(arg)
     elif cmd == "up":
-        depth -= int(arg)
+        aim -= int(arg)
 
 print(pos)
 print(depth)
 print(pos * depth)
-
