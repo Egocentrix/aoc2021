a31a290000e7ffb3337414c07be417eaa80e4c95
Reinout Nonhebel <github@rpnonhebel.nl>
2021-12-06 17:06:09 +0100

    Add day 3 part 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -12,7 +12,28 @@ epsilon = np.invert(gamma)
 
 def bits2num(bits):
     bitvalue = [2**i for i in range(11,-1,-1)]
-    return sum(bitvalue * bits)
+    return np.sum(bitvalue * bits)
 
 power = bits2num(gamma) * bits2num(epsilon)
 print("Power consumption: ", power)
+
+# Part 2
+def ox_criterion(v):
+    return sum(v) >= len(v)/2
+
+def co2_criterion(v):
+    return sum(v) < len(v)/2
+
+def filter_criterion(bits, criterion):
+    subset = bits
+    for pos in range(12):
+        subset = subset[subset[:,pos] == criterion(subset[:,pos])]
+        if len(subset) == 1:
+            break
+    return subset
+
+ox_rating = bits2num(filter_criterion(bits, ox_criterion))
+co2_rating = bits2num(filter_criterion(bits, co2_criterion))
+
+ls_rating = ox_rating * co2_rating
+print("Life support rating: ", ls_rating)
